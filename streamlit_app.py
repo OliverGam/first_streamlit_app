04fbcfe84917a9d40244aca004e991c636d3bbf8
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-22 10:33:51 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,5 +3,5 @@ import pandas
 import requests
 
 
-raw = read_csv('C:/Users/frup00090287/OneDrive - GFI/Bureau/Tuto snowflake/Fromages/Aire_production_AOP")
+raw = read_csv('C:/Users/frup00090287/OneDrive - GFI/Bureau/Tuto snowflake/Fromages/Aire_production_AOP')
 print(raw.head())
