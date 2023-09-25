614e4019bdbaf8b94aea174d88e1a732869bb6a0
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-25 14:40:00 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,4 +45,4 @@ if st.button('Voir à quoi ressemble mon fromage'):
     except:
         video_file = open('nope_1.mp4', 'rb')
         video_bytes = video_file.read()
-        st.video(video_bytes,autoplay=True)
+        st.video(video_bytes,autoplay="true")
