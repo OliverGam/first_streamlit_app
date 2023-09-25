eef47b4078e9e87aa248b1b32aa9004836082b63
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-25 13:24:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,9 @@ dep_select = st.multiselect('Quel département souhaitez-vous consulter ?',mes_f
 st.dataframe(mes_fromages_end[mes_fromages_end['Département'].isin(dep_select)][['Département','NOM_FROMAGE','LAIT']].sort_values('Département'))
 
 for a in dep_select:
-  fromage = Image.open(a||'.jpg')
-  st.image(fromage,caption = a)
+  try:
+    fromage = Image.open(a||'.jpg')
+    st.image(fromage,caption = a)
 
 
 
