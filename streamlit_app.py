63b2844d849d59fff02f69dc783dabf3b19f76b5
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-25 09:58:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,10 @@ import streamlit
 import pandas
 import requests
 
-import sys
-!{sys.executable} -m pip install fuzzywuzzy
-!{sys.executable} -m pip install python-Levenshtein
-!{sys.executable} -m pip install difflib
-
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
+import geopandas as gpd
+import folium
+
 
 brute = pd.read_csv('C:/Users/frup00090287/OneDrive - GFI/Bureau/Tuto snowflake/Fromages/Aire_production_AOP.csv',sep=';', encoding='latin-1')
