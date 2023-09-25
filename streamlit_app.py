ae7af0c3b013e66b0022012ca8c8ff74c9a2ee11
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-25 10:32:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,4 @@ import folium
 brute = pd.read_csv('https://raw.githubusercontent.com/OliverGam/first_streamlit_app/main/Aire_production_AOP.csv',sep=';',encoding='latin-1')
 data_brute = brute[['Département','Aire géographique','CI']]
 
-fromages_liste = pd.read_csv('C:/Users/frup00090287/OneDrive - GFI/Bureau/Tuto snowflake/Fromages/Streamlit_fromage.csv')
+fromages_liste = pd.read_csv('https://raw.githubusercontent.com/OliverGam/first_streamlit_app/main/Streamlit_fromage.csv')
