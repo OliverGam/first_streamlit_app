ddefbcb73aefcbd22011fa16d98ade07650ecc82
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-25 09:12:24 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit
 import pandas
 import requests
 
-dat = pandas.DataFrame({'LAT':[46.05],'LONG':[5.20]});
+dat = pandas.DataFrame({'LAT':[46.05],'LON':[5.20]});
 
 streamlit.map(dat)
