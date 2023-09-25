06d7054d52985a2bb47a65a2a99c5a6d5f67fe21
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-25 13:55:13 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,10 @@ st.dataframe(mes_fromages_end[mes_fromages_end['Département'].isin(dep_select)]
 st.button("Reset",type="primary")
 if st.button('Voir à quoi ressemble mon fromage'):
   for a in dep_select:
-    fromage = Image.open(a + '.jpg')
-    st.image(fromage,caption = a)
+    mes_from_dep = mes_fromages_end[mes_fromages_end['Département']==a]
+    for b in mes_from_dep['NOM_FROMAGE']: 
+      fromage = Image.open(b + '.jpg')
+      st.image(fromage,caption = b)
 
 
 
