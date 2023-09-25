60788802744e035658e6bd71f9dd478ecae15b91
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-25 10:52:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,22 +8,24 @@ import geopandas as gpd
 import folium
 
 
-brute = pd.read_csv('https://raw.githubusercontent.com/OliverGam/first_streamlit_app/main/Aire_production_AOP.csv',sep=';',encoding='latin-1')
-data_brute = brute[['Département','Aire géographique','CI']]
+# brute = pd.read_csv('https://raw.githubusercontent.com/OliverGam/first_streamlit_app/main/Aire_production_AOP.csv',sep=';',encoding='latin-1')
+# data_brute = brute[['Département','Aire géographique','CI']]
 
-fromages_liste = pd.read_csv('https://raw.githubusercontent.com/OliverGam/first_streamlit_app/main/Streamlit_fromage.csv')
+# fromages_liste = pd.read_csv('https://raw.githubusercontent.com/OliverGam/first_streamlit_app/main/Streamlit_fromage.csv')
 
-for a,b in data_brute.iterrows():
-    for c,d in fromages_liste.iterrows():
-        if fuzz.partial_ratio(b['Aire géographique'],d['NOM_FROMAGE'])>85:
-                      data_brute.loc[a,'NOM_DU_FROMAGE'] = fromages_liste.loc[c,'NOM_FROMAGE']
+# for a,b in data_brute.iterrows():
+#    for c,d in fromages_liste.iterrows():
+#        if fuzz.partial_ratio(b['Aire géographique'],d['NOM_FROMAGE'])>85:
+#                      data_brute.loc[a,'NOM_DU_FROMAGE'] = fromages_liste.loc[c,'NOM_FROMAGE']
 
-data_brute.drop(columns=['CI'],inplace=True)
-data_brute_merge = data_brute[~data_brute['NOM_DU_FROMAGE'].isna()].copy()
-data_brute_merge[(data_brute_merge['Aire géographique'].str.strip() != data_brute_merge['NOM_DU_FROMAGE'].str.strip())]
-mes_fromages_merge = fromages_liste.merge(data_brute_merge,how='left',left_on='NOM_FROMAGE',right_on='NOM_DU_FROMAGE').drop_duplicates()
-mes_fromages = mes_fromages_merge.drop(columns=['Aire géographique','NOM_DU_FROMAGE']).copy().reset_index(drop=True)
-mes_fromages.drop_duplicates(inplace=True)
+#data_brute.drop(columns=['CI'],inplace=True)
+#data_brute_merge = data_brute[~data_brute['NOM_DU_FROMAGE'].isna()].copy()
+#data_brute_merge[(data_brute_merge['Aire géographique'].str.strip() != data_brute_merge['NOM_DU_FROMAGE'].str.strip())]
+#mes_fromages_merge = fromages_liste.merge(data_brute_merge,how='left',left_on='NOM_FROMAGE',right_on='NOM_DU_FROMAGE').drop_duplicates()
+#mes_fromages = mes_fromages_merge.drop(columns=['Aire géographique','NOM_DU_FROMAGE']).copy().reset_index(drop=True)
+#mes_fromages.drop_duplicates(inplace=True)
+
+# mes_fromgaes_end = pd.read_csv('https://raw.githubusercontent.com/OliverGam/first_streamlit_app/main/Streamlit_fromage.csv
 
 st.title('Les fromages AOP')
 st.dataframe(mes_fromages)
