f5dce87df576a706a085789dd4ef48906579422d
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-25 10:20:11 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,4 @@ import folium
 
 brute = pd.read_csv('https://raw.githubusercontent.com/OliverGam/first_streamlit_app/main/Aire_production_AOP.csv',sep=';',encoding='latin-1')
 data_brute = brute[['Département','Aire géographique','CI']]
-st.swrite(data_brute.head())
+st.write(data_brute.head())
