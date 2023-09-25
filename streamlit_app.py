e6216902b335a8927abf4fcaaaa25ba84a3cf99a
OliverGam <144343161+OliverGam@users.noreply.github.com>
2023-09-25 09:53:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,12 @@ import streamlit
 import pandas
 import requests
 
-dat = pandas.DataFrame({'LAT':[46.05],'LON':[5.20]});
+import sys
+!{sys.executable} -m pip install fuzzywuzzy
+!{sys.executable} -m pip install python-Levenshtein
+!{sys.executable} -m pip install difflib
 
-streamlit.map(dat)
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
+
+brute = pd.read_csv('C:/Users/frup00090287/OneDrive - GFI/Bureau/Tuto snowflake/Fromages/Aire_production_AOP.csv',sep=';', encoding='latin-1')
